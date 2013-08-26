c80d2cdc93aae203a1af0a2becd66d7b63e93b3c
Arnaud Lemaire <dev@norad.fr>
2013-08-26 13:12:46 +0200

    add missing files

---
+++ web-bootstrap/karma.conf.js
@@ -0,0 +1,38 @@
+  module.exports = function(config) {
+    config.set({
+    	basePath : '',
+    	files : [
+    	         'src/main/web/bower_components/angular/angular.js',
+    	         'src/main/web/bower_components/angular-mocks/angular-mocks.js',
+    	         'src/main/web/scripts/*.js',
+    	         'src/main/web/scripts/**/*.js',
+    	         'src/test/web/mock/**/*.js',
+    	         'src/test/web/spec/**/*.js'
+    	       ],
+    	 frameworks : ["jasmine"],
+    	 exclude : [],
+    	 reporters : ['progress'],
+
+    	 port : 8080,
+    	 runnerPort : 9100,
+
+    	 colors : true,
+	    logLevel : config.LOG_INFO,
+	    autoWatch : false,
+	    browsers : ['PhantomJS'],
+	    captureTimeout : 5000,
+	    singleRun : false
+
+    });
+  };
+
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
---
+++ web-bootstrap/src/main/web/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jaxrs-doc')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
